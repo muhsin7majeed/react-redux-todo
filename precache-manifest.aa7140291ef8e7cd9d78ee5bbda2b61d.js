20cfa6599391ce134979adb488708a6676b13eb7
muhsin7majeed <muhsin7ak@gmail.com>
2019-10-24 18:02:13 +0530

    Updates

---
+++ precache-manifest.aa7140291ef8e7cd9d78ee5bbda2b61d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31460a4ee379b28bcf37bde30c64704b",
+    "url": "/react-redux-todo/index.html"
+  },
+  {
+    "revision": "95d978479ac4c379a923",
+    "url": "/react-redux-todo/static/css/main.efa92037.chunk.css"
+  },
+  {
+    "revision": "a9953b297213bf98b9e2",
+    "url": "/react-redux-todo/static/js/2.70f0fcf9.chunk.js"
+  },
+  {
+    "revision": "95d978479ac4c379a923",
+    "url": "/react-redux-todo/static/js/main.1cd79b40.chunk.js"
+  },
+  {
+    "revision": "af89693c05b1ddf93ee3",
+    "url": "/react-redux-todo/static/js/runtime-main.c2979ddc.js"
+  }
+]);
